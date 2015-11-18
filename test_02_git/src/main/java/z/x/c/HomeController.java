4a6afaead447daefae45cd696bac00a9de1245cb
pz <pz@ip-192-168-222-1.is74.loc>
2015-11-18 21:59:43 +0500

    11-1-br01

--- test_02_git/src/main/java/z/x/c/HomeController.java
+++ test_02_git/src/main/java/z/x/c/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
-	 * 11-1
+	 * 11-1-br01
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
