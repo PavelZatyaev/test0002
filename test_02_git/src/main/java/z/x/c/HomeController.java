3875a6394312a685e6776fb7150a062d12d720b3
pzatyaev <pz231270@yandex.ru>
2015-11-19 12:48:17 +0500

    work!!!

--- test_02_git/src/main/java/z/x/c/HomeController.java
+++ test_02_git/src/main/java/z/x/c/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
-	 * 11
+	 * 11 work!!!
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
