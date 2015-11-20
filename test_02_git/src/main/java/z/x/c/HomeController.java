eb269eca06c1f3b064b5cdaf1a19f1520e52500f
pzatyaev <pz231270@yandex.ru>
2015-11-20 16:11:18 +0500

    check it!!!!!!

--- test_02_git/src/main/java/z/x/c/HomeController.java
+++ test_02_git/src/main/java/z/x/c/HomeController.java
@@ -20,6 +20,7 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
+	 * checkit!!!
 	 * 11 work!!!-01
 	 * Simply selects the home view to render by returning its name.
 	 */
